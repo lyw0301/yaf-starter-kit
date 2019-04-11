e34c1a3dd7ef4a692c21253b6f83feed242194ac
panli <panli0208@gmail.com>
2019-04-11 11:47:30 +0800

    add mongod demo

--- app/controllers/Demo/Mongo.php
+++ app/controllers/Demo/Mongo.php
@@ -9,16 +9,19 @@ class Demo_MongoController extends BaseController
 {
     public function handle()
     {
+        // 连接
         $uri = "mongodb://root:root@localhost:27017";
         $client = new MongoDB\Client($uri);
         $collection = $client->demo->website;
 
+        // 插入操作
         $result = $collection->insertOne([
             'name' => 'MongoChina',
             'url' => 'http://www.mongochina.com',
         ]);
         echo "Inserted with Object ID '{$result->getInsertedId()}'";
 
+        // 获取结果
         $result = $collection->find()->toArray();
         print_r($result);
     }
